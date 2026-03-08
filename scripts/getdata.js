1f338839bb7f0fc297572e3a69120974247041d5
rhamermesh <rhamermesh@brandeis.edu>
2026-03-08 05:31:35 +0000

    sync

--- scripts/getdata.js
+++ scripts/getdata.js
@@ -7,7 +7,7 @@ export async function getData(word) {
 // function to get the default translation of a word
 async function getDefinition(word) {
         try {
-        const url = "/api/words/" + word;
+        const url = "https://www.sefaria.org/api/words/" + word;
         // get the data from the url
         const data = await fetch(url);
         const jsonData = await data.json();
@@ -22,7 +22,7 @@ async function getDefinition(word) {
 
 // function to get 
 async function getVerses(wordtoSearch){ 
-        const url = "/api/search-wrapper";
+        const url = "https://www.sefaria.org/api/search-wrapper";
   
         const body = {
                 "query": wordtoSearch,
@@ -44,62 +44,77 @@ async function getVerses(wordtoSearch){
                 const data = await response.json();
                 const verses = [];
                 for (let i = 0; i < data.hits.hits.length; i++) {
-                        verses.push({ [data.hits.hits[i]._id]: data.hits.hits[i].highlight.naive_lemmatizer });
+                        verses.push({ [data.hits.hits[i]._id]: data.hits.hits[i].highlight.naive_lemmatizer[0] });
                 }
-                return verses;
+                return await cleanData(verses);
         } catch (err) {
                 console.error("Search Error:", err);
         }
 }
 
-// // testing the function yt to file
+// clean the data to make sure we only have one version of any text
+async function cleanData(verses) {
+        const uniqueVerses = [];
+        const seen = new Set();
+        for (const verse of verses) {
+                const key = Object.keys(verse)[0];
+                const newKey = key.split("(")[0]; // remove the version part
+                if (!seen.has(newKey)) {
+                        seen.add(newKey);
+                        uniqueVerses.push(verse);
+                }
+        }
+        return uniqueVerses;
+}
+
+// testing the function not to file
+async function main() {
+        const word = "אמר";
+
+
+        // const url = "https://www.sefaria.org/api/search-wrapper" + word;
+        // const testdata = await fetch(url);
+        // const jsonData = await testdata.json();
+        // console.log(jsonData);
+
+        const data = await getData(word);
+        console.log(data.word + ": " + data.definition + " - " ); 
+                // for (let i = 0; i < data.verses.length; i++) { 
+                //         console.log(data.verses[i]);
+                // }
+        
+}
+main();
+
+
+// import fs from 'fs';
+// // testing function to json file
 // async function main() {
-//         const word = "אמר";
+//         const result = [];
+//         const words = ["אמר", "דבר", "עשה", "ראה", "שמע", "הלך", "בא", "נתן", "לקח", "אמרו"];
+//         for(let i = 0; i < words.length; i++) {
+//                 const data = await getData(words[i]);
+//                 const jsonData = JSON.stringify(data, null, 2);
+//                 console.log(jsonData);
+//                 result.push(data);
+//         }
 
-
-//         // const url = "https://www.sefaria.org/api/search-wrapper" + word;
-//         // const testdata = await fetch(url);
-//         // const jsonData = await testdata.json();
-//         // console.log(jsonData);
-
-//         const data = await getData(word);
-//         console.log(data.word + ": " + data.definition + " - " ); 
-//                 for (let i = 0; i < data.verses.length; i++) { 
-//                         console.log(data.verses[i]);
+//         const finalJson = {cards : result};
+//         console.log(JSON.stringify(finalJson, null, 2));
+        
+//         fs.writeFile('example.json', JSON.stringify(finalJson, null, 2), (err) => {
+//                 if (err) {
+//                         console.error('Error writing to file:', err);
+//                 } else {
+//                         console.log('Data successfully written to output.json');
 //                 }
+//         });
         
 // }
-// main();
 
-
-import fs from 'fs';
-// testing function to json file
-async function main() {
-        const result = [];
-        const words = ["אמר", "דבר", "עשה", "ראה", "שמע", "הלך", "בא", "נתן", "לקח", "אמרו"];
-        for(let i = 0; i < words.length; i++) {
-                const data = await getData(words[i]);
-                const jsonData = JSON.stringify(data, null, 2);
-                console.log(jsonData);
-                result.push(data);
-        }
-
-        const finalJson = {cards : result};
-        console.log(JSON.stringify(finalJson, null, 2));
-        
-        fs.writeFile('example.json', JSON.stringify(finalJson, null, 2), (err) => {
-                if (err) {
-                        console.error('Error writing to file:', err);
-                } else {
-                        console.log('Data successfully written to output.json');
-                }
-        });
-        
-}
-
-const test = false;
-if (test) {
-        main();
-}
+// const test = true;
+// if (test) {
+//         main();
+// }
 
 
