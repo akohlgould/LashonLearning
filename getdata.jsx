a3f817665b314a002193d8e1823286655efaf2cb
rhamermesh <rhamermesh@brandeis.edu>
2026-03-08 01:56:54 +0000

    added the getadata file with skeleton

---
+++ getdata.jsx
@@ -0,0 +1,20 @@
+import sefaria from '@api/sefaria';
+data = "";
+function getData(a) {
+        // todo
+}
+
+function getDefenition{a}{
+        // get the defention of the word
+}
+
+function getRandomVerse(a){
+        // get a random verse from the tanakh
+}
+
+function shapeData(a){
+        // shape the data to be used in the app
+}
+
+
+export default getData;
