11950e180bd7a04938a2df4265cfae685951d926
onegkg <onegkg@gmail.com>
2026-03-07 23:41:18 -0500

    Anki export is working now

--- scripts/AnkiExport.js
+++ scripts/AnkiExport.js
@@ -1,6 +1,6 @@
 import { getData } from "./getdata.js";
 
-export async function exportToAnki(data) {
+async function convertToTSV(data) {
   const cards = data.cards;
   let tsv = "";
   for (const card of cards) {
@@ -11,6 +11,18 @@ export async function exportToAnki(data) {
   return tsv;
 }
 
+export async function exportToAnki(data) {
+  const tsv = await convertToTSV(data);
+  const blob = new Blob([tsv], { type: "text/tab-separated-values" });
+
+  const url = URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "flashcards.tsv";
+  a.click();
+  URL.revokeObjectURL(url);
+}
+
 function randomInArray(array) {
   return array[Math.floor(Math.random() * (array.length - 1))];
 }
@@ -18,5 +30,5 @@ function randomInArray(array) {
 async function main() {
   const entry = await getData("תורה");
   const data = { cards: [entry] };
-  console.log(exportToAnki(data));
+  console.log(convertToTSV(data));
 }
