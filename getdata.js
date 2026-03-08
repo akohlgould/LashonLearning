46c8a4dee96c251cf8d9cf6a049ab16fc263601f
rhamermesh <rhamermesh@brandeis.edu>
2026-03-08 02:52:27 +0000

    getdata basic version is working

--- getdata.js
+++ getdata.js
@@ -1,24 +1,32 @@
 
 // function to process a word using all the functions below and return the data to be used in the app
-function getData(a) {
-        data = "";
-
-        return data;        
+async function getData(word) {
+        const url = "https://www.sefaria.org/api/words/" + word;
+        // get the data from the url
+        const data = await fetch(url);
+        const jsonData = await data.json();
+        return { word: word, definition: getDefinition(jsonData), randomVerse: getRandomVerse(jsonData) };
 }
 
+
 // function to get the default translation of a word
-function getDefenition(a){
-        // get the defention of the word
+function getDefinition(word){
+        // get the definition of the word
+        const definition = word[0].content.senses[0].definition;
+        return definition;
+        
 }
 
 // function to get 
-function getRandomVerse(a){
+function getRandomVerse(word){
         // get a random verse from the tanakh
+        return word[0].refs[0];
 }
 
-function shapeData(a){
-        // shape the data to be used in the app
-}
-
-
-export default getData;
+// // testing the function
+// async function main() {
+//         data = await getData("תורה");
+//         console.log(data.word + ": " + data.definition + " - " + data.randomVerse); 
+        
+// }
+// main();
