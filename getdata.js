8e9754dc581893f3f3d3de3069853043a4dead28
rhamermesh <rhamermesh@brandeis.edu>
2026-03-08 02:16:59 +0000

    fixed curly brackets

---
+++ getdata.js
@@ -0,0 +1,24 @@
+
+// function to process a word using all the functions below and return the data to be used in the app
+function getData(a) {
+        data = "";
+
+        return data;        
+}
+
+// function to get the default translation of a word
+function getDefenition(a){
+        // get the defention of the word
+}
+
+// function to get 
+function getRandomVerse(a){
+        // get a random verse from the tanakh
+}
+
+function shapeData(a){
+        // shape the data to be used in the app
+}
+
+
+export default getData;
