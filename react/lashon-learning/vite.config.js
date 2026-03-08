245552592d145f7d1224ce2ae3aaf096c5ba85d2
Brandeisian <AdamGould@brandeis.edu>
2026-03-08 03:27:15 -0400

    for pages

--- react/lashon-learning/vite.config.js
+++ react/lashon-learning/vite.config.js
@@ -2,10 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  // 1. Set base to your GitHub Repository name
   base: '/LashonLearning/',
   server: {
     proxy: {
@@ -15,8 +13,4 @@ export default defineConfig({
       },
     },
   },
-  // 2. Ensure the build output goes to the standard 'dist' folder
-  build: {
-    outDir: 'dist',
-  }
 })
