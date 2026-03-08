8b81b502d5e260d62d7248db0237330971949f49
adameheaney <adamheaney321@gmail.com>
2026-03-07 21:58:14 -0500

    h's react app

---
+++ react/lashon-learning/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <span>ס</span>
+    </header>
+  );
+};
+
+export default Header;
---
+++ react/lashon-learning/src/components/MainContent.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function MainContent() {
+  return (
+    <div>
+      <h1>Main Content</h1>
+      <p>Welcome to the main content area.</p>
+    </div>
+  );
+}
