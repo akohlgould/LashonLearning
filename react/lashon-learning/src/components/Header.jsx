521885dfe1f3903c1c72db8d090c8f7830797185
adameheaney <adamheaney321@gmail.com>
2026-03-08 03:31:04 -0400

    nice header

--- react/lashon-learning/src/components/Header.jsx
+++ react/lashon-learning/src/components/Header.jsx
@@ -4,8 +4,9 @@ import { FaGlobe } from "react-icons/fa";
 import { FaUserCircle } from "react-icons/fa";
 import { FaQuestionCircle } from "react-icons/fa";
 import { CgMenuGridO } from "react-icons/cg";
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 const Header = () => {
+  const { pathname } = useLocation();
   return (
     <header className="flex flex-1 px-7 align-baseline place-items-center gap-4 bg-white border-b-[#5794b1] border-b-4 h-16">
       <h1 className="text-[30px] font-hebrew-serif text-primary">ל</h1>
@@ -13,8 +14,8 @@ const Header = () => {
         Lashon Learning
       </h1>
       <div className="flex ml-6 gap-8 font-semibold text-[#666666]">
-        <Link to="/"><span className="">home</span></Link>
-        <Link to="/wordlist"><span className="">wordlist</span></Link>
+        <Link to="/" className={pathname === "/" ? "text-primary" : ""}><span>Home</span></Link>
+        <Link to="/wordlist" className={pathname === "/wordlist" ? "text-primary" : ""}><span>Word List</span></Link>
       </div>
 
       <div className="ml-auto text-[#666666] relative font-semibold flex gap-5 place-items-center">
